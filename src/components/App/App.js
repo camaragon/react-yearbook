d2afe8c5adfacf9667ef4b3085ccc85abb80d6fc
Cameron Aragon <caragon4695@gmail.com>
2021-02-10 14:31:27 -0700

    Add student data to the state of App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      staff: people.staff
+      staff: people.staff,
+      students: people.students
     }
   }
 
